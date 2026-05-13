daf046a0d797163e8b0a7639eef35d9452fb7e1c
suhani11081341 <suhani11081341@gmail.com>
2026-05-13 14:52:39 +0530

    help feature

--- flask_project/app.py
+++ flask_project/app.py
@@ -7,5 +7,10 @@ def home():
     return render_template("home.html")
 
 
+
+@app.route("/help")
+def help_page():
+    return render_template("help.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
