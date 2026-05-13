1819c06f81365a6d9592716bb2f9505415f765e8
suhani11081341 <suhani11081341@gmail.com>
2026-05-13 14:48:34 +0530

    login feature

--- flask_project/app.py
+++ flask_project/app.py
@@ -7,5 +7,11 @@ def home():
     return render_template("home.html")
 
 
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
